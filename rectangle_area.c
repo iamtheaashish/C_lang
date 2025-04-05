b96ce4f5e9a7b1434c068d59bf16a3bea76d9406
Aashish Raj <iamtheaashish@gmail.com>
2025-04-05 09:13:15 +0530

    Update rectangle_area.c

--- rectangle_area.c
+++ rectangle_area.c
@@ -1,1 +1,18 @@
+int main() {
+    int length, breadth;
+    // Ask user to enter length and breadth
+    printf("Enter length of rectangle: ");
+    scanf("%d", &length);
 
+    printf("Enter breadth of rectangle: ");
+    scanf("%d", &breadth);
+
+    // Calculate area
+    int area = length * breadth;
+
+    // Display Result
+    printf("Area of sexy Rectangle is = %d\n", area);
+    return 0;  // tells the OS the program ended successfully
+}
+
+// i have used chatgpt but i have understood whatever i have written. I am learning C before python. Do hard things first.
