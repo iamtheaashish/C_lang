7c86ae0de1fe434298ed48231c698aba21a1d220
Aashish Raj <iamtheaashish@gmail.com>
2025-04-05 09:28:27 +0530

    Create circle_area.c

---
+++ circle_area.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  // for input/output
+
+int main() {
+    int radius;
+    // Ask user to enter radius
+    printf("Enter radius of circle: ");
+    scanf("%d", &radius);
+
+    // Calculate area
+    int area = 3.14 * radius * radius;
+
+    // Display Result
+    printf("Area of ur boobs is = %d\n", area);
+    return 0;  // tells the OS the program ended successfully
+}
