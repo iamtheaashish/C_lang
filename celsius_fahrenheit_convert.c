badccdd4602cf821108324f8335cd7b32f05d43e
Aashish Raj <iamtheaashish@gmail.com>
2025-04-07 09:08:49 +0530

    Update celsius_fahrenheit_convert.c

--- celsius_fahrenheit_convert.c
+++ celsius_fahrenheit_convert.c
@@ -2,13 +2,13 @@
 int main() {
     int celsius;
     // Ask user to enter Temperature in Celsius
-    printf("Enter temperature in celsius : ");
+    printf("Enter her temperature in celsius \xF0\x9F\x94\xA5\n: ");
     scanf("%d", &celsius);
 
     // Convert Celcius to Fahrenheit
     float fahrenheit = (9.0/5.0)*celsius + 32;
 
     // Display Result
-    printf("Babe U R this hot in Fahrenheit = %.2f\n", fahrenheit);
+    printf("She is this hot in Fahrenheit \xF0\x9F\xA5\xB5\n : %.2f\n", fahrenheit);
     return 0;  // tells the OS the program ended successfully
 }
